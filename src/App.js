223a15a78c9cc1d2715eabb6c7291884dff4a437
Yeonwoo Ga <vjk4874@naver.com>
2022-10-06 11:02:49 +0900

    시험 답안

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function ListBox(props){
+  return (
+    <div className='todo-box'>
+      {props.list}
+    </div>
+  )
+}
+
+
 
 function App() {
+  const [todo,setTodo] = useState([])
+  const [change,setChange] = useState('')
+
+  const ChangeValue = (e) =>{
+    setChange(e.target.value)
+  }
+
+  const AddTodo = () => {
+    const copy = [...todo]
+    setTodo([...copy,change])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='full-box'>
+      <div>
+        <input type='text' value={change} onChange={ChangeValue}/>
+        <button onClick={AddTodo}>추가하기</button>
+      </div>
+      <h1>Todo List</h1>
+      <div className='list-box'>
+        {
+          todo.map((todos) => {
+            return <ListBox key={todos} list={todos}/>
+          })
+        }
+      </div>
     </div>
+
+    </>
   );
 }
 
